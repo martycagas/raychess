31881d1785db0923589d07548442d9b5ee20a9d1
Marty Cagas <martycagas@users.noreply.github.com>
2022-08-23 21:53:38 +0200

    Added an empty Game class

---
+++ src/raychess_core/game.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file    game.cpp
+ *
+ * @brief   The core code of a chess game.
+ *
+ * @section DESCRIPTION
+ *
+ * This library focuses on providing the infrastructure for a chess game.
+ */
+
+#include "game.hpp"
+
+using namespace raychess;
---
+++ src/raychess_core/game.hpp
@@ -0,0 +1,15 @@
+/**
+ * @file    game.cpp
+ *
+ * @brief   The core code of a chess game.
+ *
+ * @section DESCRIPTION
+ *
+ * This library focuses on providing the infrastructure for a chess game.
+ */
+
+#pragma once
+
+namespace raychess {
+
+}
