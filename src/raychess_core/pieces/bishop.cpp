1bd186a34849b970d1821ce7095977f0f1aeff27
Marty Cagas <martycagas@users.noreply.github.com>
2022-08-25 17:48:36 +0200

    Added the bishop chess piece

---
+++ src/raychess_core/pieces/bishop.cpp
@@ -0,0 +1,35 @@
+/**
+ * @file    bishop.cpp
+ *
+ * @brief   Class representing a bishop.
+ *
+ * @section DESCRIPTION
+ *
+ * Class representing a bishop chess piece with its own unique behaviour.
+ */
+
+#include "bishop.hpp"
+
+using namespace raychess;
+
+std::vector<Position2D> Bishop::GetMoves(void) noexcept
+{
+    std::vector<Position2D> moves;
+    moves.reserve(14);
+
+    Position2D new_position;
+
+    for (int i = -1; i <= 1; i += 2) {
+        for (int j = -1; j <= 1; j += 2) {
+            // No need for a check here, the loop condition ensures that i and j are both either -1,
+            // or 1.
+            new_position = position_ + Position2D(i, j);
+            while (!new_position.IsOutOfBounds(8, 8)) {
+                moves.push_back(new_position);
+                new_position += Position2D(i, j);
+            }
+        }
+    }
+
+    return moves;
+}
---
+++ src/raychess_core/pieces/bishop.hpp
@@ -0,0 +1,53 @@
+/**
+ * @file    bishop.hpp
+ *
+ * @brief   Class representing a bishop.
+ *
+ * @section DESCRIPTION
+ *
+ * Class representing a bishop chess piece with its own unique behaviour.
+ */
+
+#pragma once
+
+#include <vector>
+
+#include "piece_base.hpp"
+#include "pos2d.hpp"
+
+namespace raychess
+{
+    class Bishop : public PieceBase
+    {
+    public:
+        /**
+         * @brief       Using the base class constructor.
+         *
+         * @see         PieceBase::PieceBase(PieceColour colour, Position2D position)
+         */
+        using PieceBase::PieceBase;
+
+        /**
+         * @brief       Get a vector of all possible moves for the piece.
+         *
+         * @see         PieceBase::GetMoves()
+         *
+         * @return      A vector of all possible moves for the piece.
+         */
+        std::vector<Position2D> GetMoves(void) noexcept override;
+
+        /**
+         * @brief       Using the default implementation of the GetAttackOnlyMoves method.
+         *
+         * @see         PieceBase::GetAttackOnlyMoves(void)
+         */
+        using PieceBase::GetAttackOnlyMoves;
+
+        /**
+         * @brief       Using the default implementation of the Move method.
+         *
+         * @see         PieceBase::Move(Position2D new_position)
+         */
+        using PieceBase::Move;
+    };
+}  // namespace raychess
